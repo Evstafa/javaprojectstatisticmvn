4d5d9a2f876914b161d92bc474511acb5d1cf8ee
Anna evstafa <a.evstafa@gmail.com>
2023-01-26 00:18:43 +0300

    srm

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -14,4 +14,15 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void findMaxMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 90, 6, 11, 11, 12};
+        long expected = 90;
+
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
 }
